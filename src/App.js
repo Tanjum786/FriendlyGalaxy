da65cd054ee7fca3e1751a6da7ce149f0a3a132a
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-05-31 09:08:15 +0530

    feat :  imported toast container in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
-import { Login, Signup } from "./Pages";
 import { Routers } from "./Routers/Routers";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
     <>
+     <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        theme="dark"
+        draggable
+        pauseOnHover
+      />
       <div className="main-continer">
         <Routers />
       </div>
